4ecbb9fa020dd27d5aabeb6ccdd882efa9871b83
Amna Saeed <amnas9428@gmail.com>
2026-01-09 10:32:20 +0500

    change

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const REACT_APP_API_URL = "https://complaint-system-backend.vercel.app/api/"
-const REACT_APP_API_URL = "http://192.168.1.7:3000/api/"
+const REACT_APP_API_URL = "http://192.168.1.61:3000/api/"
 const API_URL = REACT_APP_API_URL;
 
 // Function to handle blob file download
